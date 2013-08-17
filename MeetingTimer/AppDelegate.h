3d3fed92350e5e9472042b4b96f120255ff45e60
wkymtnb <yamamoto@knuckle.local>
2013-08-17 16:51:06 +0900

    Initial Commit

---
+++ MeetingTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MeetingTimer
+//
+//  Created by Takahiro Yamamoto on 2013/08/17.
+//  Copyright (c) 2013年 MyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeetingTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MeetingTimer
+//
+//  Created by Takahiro Yamamoto on 2013/08/17.
+//  Copyright (c) 2013年 MyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
